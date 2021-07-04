4b64413734a8fc0f12ed0bf8e88ac8e8d95699dc
Alejandro Mery <amery@jpi.io>
2021-07-04 03:37:24 +0100

    shexp: add NewParser() shortcut using the default Getenv Expander
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- shexp/global.go
+++ shexp/global.go
@@ -17,3 +17,15 @@ func ExpandBytes(b []byte) (string, error) {
 func Expand(in io.Reader) (string, error) {
 	return envExpander.Expand(in)
 }
+
+func NewStringParser(s string, args ...interface{}) *Parser {
+	return envExpander.NewStringParser(s, args...)
+}
+
+func NewBytesParser(b []byte) *Parser {
+	return envExpander.NewBytesParser(b)
+}
+
+func NewParser(in io.Reader) *Parser {
+	return envExpander.NewParser(in)
+}
